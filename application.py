20a4521b0d87fcbcdcb8eb113025a55a40b96f49
Ayush Dewan <ayushdewan02@gmail.com>
2020-09-12 12:32:58 -0400

    templates

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# Login Page
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
+
+# Dashboard Page
+@app.route('/user/<username>')
+def show_dash(username):
+    return render_template("dashboard.html", username=username)
