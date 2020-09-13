2c22d1a4a6fac7f2c4ae136be56b08de5902c048
Cathy <cathyhou2002@gmail.com>
2020-09-12 17:43:41 -0700

    updates

--- application.py
+++ application.py
@@ -1,34 +1,45 @@
 from flask import Flask, render_template
-import pymysql.cursors
 app = Flask(__name__)
 
-# Connect to the database
-connection = pymysql.connect(host='35.238.255.61',
-                             user='root',
-                             password='claimcart',
-                             db='claims',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+import functools
+import json
+import os
 
-# Login Page
+import flask
+
+from authlib.client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
+
+import google_auth
+
+app = flask.Flask(__name__)
+app.secret_key = "secret"
+
+app.register_blueprint(google_auth.app)
+
 @app.route('/')
 def index():
+    if google_auth.is_logged_in():
+        return render_template("home.html")
     return render_template("login.html")
 
-# Dashboard Page
+def getEmail():
+    user_info = google_auth.get_user_info()
+    return user_info['email']
+    
+@app.route('/list')
+def list():
+    return render_template("dashboard.html")
+
+@app.route('/family')
+def family():
+    return render_template("family.html")
+
+@app.route('/upload')
+def upload():
+    return render_template("upload.html")
+
 @app.route('/user/<username>')
 def show_dash(username):
-    return render_template("dashboard.html", username=username.capitalize(), table=[])
-
-# List Page
-@app.route('/list/<username>')
-def show_list(username):
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `*` FROM `items` WHERE `user`=%s"
-        cursor.execute(sql, ('ayushdewan02@gmail.com',))
-        result = cursor.fetchall()
-        for i in result:
-            i['price'] = "$%0.2f" % i['price']
-    print(result)
-    return render_template("dashboard.html", username=username.capitalize(), table=result)
+    return render_template("dashboard.html", username=username)
